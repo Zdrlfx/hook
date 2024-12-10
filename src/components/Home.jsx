fd1aa782dc2a18659a9f27f99fb33ff0ebf0f0fd
Sweta <swetasuwal001@gmail.com>
2024-12-10 23:10:28 +0545

    modify CTA div in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const Home = () => {
   return (
@@ -16,7 +17,34 @@ const Home = () => {
           className="w-1/2 h-auto absolute bottom-0 right-1"
         />
       </div>
-      <div className="bg-indigo-500 text-white rounded-3xl p-6 shadow-lg flex flex-col items-center md:items-start w-[calc(72%)] h-full "></div>
+      <div className="bg-gradient-to-br from-indigo-500 to-indigo-700 text-white rounded-3xl p-6 shadow-lg flex flex-col items-center md:items-start w-[calc(72%)] h-full ">
+        <h2 className="text-2xl font-bold mb-4 text-center md:text-left">
+          Hi Unique, Welcome back!
+        </h2>
+        <p className="text-sm mb-4 text-center md:text-left">
+          Browse through the available halls for your events and secure your
+          booking in just a few clicks!
+        </p>
+        <div className="flex items-center justify-center">
+          <button className="flex items-center bg-black text-white font-medium px-4 py-2 rounded-full shadow-lg hover:bg-gray-800 transition duration-300 text-xs">
+            Join Now
+            <span className="ml-2 flex items-center justify-center w-5 h-5 bg-white text-black rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                class="size-4"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
